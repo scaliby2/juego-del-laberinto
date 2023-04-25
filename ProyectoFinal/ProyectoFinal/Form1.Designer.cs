4959d1b63a2c3741389e33654eaad0559c5b1036
scaliby2 <111079865+scaliby2@users.noreply.github.com>
2023-04-25 11:04:24 -0600

    Add files via upload

---
+++ ProyectoFinal/ProyectoFinal/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ProyectoFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblObstaculo = new System.Windows.Forms.Label();
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.lblInstrucciones = new System.Windows.Forms.Label();
+            this.lblJugador = new System.Windows.Forms.Label();
+            this.lblReinicio = new System.Windows.Forms.Label();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.labelInfo1 = new System.Windows.Forms.Label();
+            this.txtbxLaberinto = new System.Windows.Forms.TextBox();
+            this.lblLeyenda = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblObstaculo
+            // 
+            this.lblObstaculo.AutoSize = true;
+            this.lblObstaculo.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObstaculo.Location = new System.Drawing.Point(82, 122);
+            this.lblObstaculo.Name = "lblObstaculo";
+            this.lblObstaculo.Size = new System.Drawing.Size(132, 18);
+            this.lblObstaculo.TabIndex = 17;
+            this.lblObstaculo.Text = "X  >>> Obstaculo";
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurno.Location = new System.Drawing.Point(137, 350);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(125, 18);
+            this.lblTurno.TabIndex = 16;
+            this.lblTurno.Text = "Turno actual: ";
+            // 
+            // lblInstrucciones
+            // 
+            this.lblInstrucciones.AutoSize = true;
+            this.lblInstrucciones.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstrucciones.Location = new System.Drawing.Point(59, 42);
+            this.lblInstrucciones.Name = "lblInstrucciones";
+            this.lblInstrucciones.Size = new System.Drawing.Size(614, 20);
+            this.lblInstrucciones.TabIndex = 15;
+            this.lblInstrucciones.Text = "Instrucciones:  Mueve a tu personaje \"J\" con las flechas de tu teclado.";
+            // 
+            // lblJugador
+            // 
+            this.lblJugador.AutoSize = true;
+            this.lblJugador.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJugador.Location = new System.Drawing.Point(82, 247);
+            this.lblJugador.Name = "lblJugador";
+            this.lblJugador.Size = new System.Drawing.Size(111, 18);
+            this.lblJugador.TabIndex = 14;
+            this.lblJugador.Text = "J  >>> Jugador";
+            // 
+            // lblReinicio
+            // 
+            this.lblReinicio.AutoSize = true;
+            this.lblReinicio.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReinicio.Location = new System.Drawing.Point(82, 213);
+            this.lblReinicio.Name = "lblReinicio";
+            this.lblReinicio.Size = new System.Drawing.Size(195, 18);
+            this.lblReinicio.TabIndex = 13;
+            this.lblReinicio.Text = "R  >>> Reregesas al inicio";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinal.Location = new System.Drawing.Point(82, 182);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(83, 18);
+            this.lblFinal.TabIndex = 12;
+            this.lblFinal.Text = "F  >>> Final";
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicio.Location = new System.Drawing.Point(82, 151);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(85, 18);
+            this.lblInicio.TabIndex = 11;
+            this.lblInicio.Text = "I  >>> Inicio";
+            // 
+            // labelInfo1
+            // 
+            this.labelInfo1.AutoSize = true;
+            this.labelInfo1.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.labelInfo1.Location = new System.Drawing.Point(82, 122);
+            this.labelInfo1.Name = "labelInfo1";
+            this.labelInfo1.Size = new System.Drawing.Size(0, 16);
+            this.labelInfo1.TabIndex = 10;
+            // 
+            // txtbxLaberinto
+            // 
+            this.txtbxLaberinto.Enabled = false;
+            this.txtbxLaberinto.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxLaberinto.Location = new System.Drawing.Point(377, 93);
+            this.txtbxLaberinto.Multiline = true;
+            this.txtbxLaberinto.Name = "txtbxLaberinto";
+            this.txtbxLaberinto.Size = new System.Drawing.Size(284, 275);
+            this.txtbxLaberinto.TabIndex = 9;
+            // 
+            // lblLeyenda
+            // 
+            this.lblLeyenda.AutoSize = true;
+            this.lblLeyenda.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLeyenda.Location = new System.Drawing.Point(68, 79);
+            this.lblLeyenda.Name = "lblLeyenda";
+            this.lblLeyenda.Size = new System.Drawing.Size(78, 18);
+            this.lblLeyenda.TabIndex = 18;
+            this.lblLeyenda.Text = "Leyenda:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(71, 290);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 27);
+            this.btnStart.TabIndex = 19;
+            this.btnStart.Text = "Iniciar Juego";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(750, 428);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblLeyenda);
+            this.Controls.Add(this.lblObstaculo);
+            this.Controls.Add(this.lblTurno);
+            this.Controls.Add(this.lblInstrucciones);
+            this.Controls.Add(this.lblJugador);
+            this.Controls.Add(this.lblReinicio);
+            this.Controls.Add(this.lblFinal);
+            this.Controls.Add(this.lblInicio);
+            this.Controls.Add(this.labelInfo1);
+            this.Controls.Add(this.txtbxLaberinto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblObstaculo;
+        private System.Windows.Forms.Label lblTurno;
+        private System.Windows.Forms.Label lblInstrucciones;
+        private System.Windows.Forms.Label lblJugador;
+        private System.Windows.Forms.Label lblReinicio;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.Label labelInfo1;
+        private System.Windows.Forms.TextBox txtbxLaberinto;
+        private System.Windows.Forms.Label lblLeyenda;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ ProyectoFinal/ProyectoFinal/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class Form1 : Form
+    {
+        char[,] aMapa = new char[10, 10];
+        int[] pInicial = new int[2];
+        int[] pJugador = new int[2];
+        int[] pReinicio = new int[2];
+        int[] pFinal = new int[2];
+        static int maxTurnos = 10;
+        int iTurno = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            StreamReader srReader = new StreamReader("mapa.txt");
+
+            while (!srReader.EndOfStream)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    var linea = srReader.ReadLine().ToCharArray();
+                    for (int i = 0; i <= 9; i++)
+                    {
+                        aMapa[j, i] = linea[i];
+                        if (linea[i] == 'I')
+                        {
+                            pInicial[0] = j;
+                            pInicial[1] = i;
+                        }
+                        else if (linea[i] == 'R')
+                        {
+                            pReinicio[0] = j;
+                            pReinicio[1] = i;
+                        }
+                        else if (linea[i] == 'F')
+                        {
+                            pFinal[0] = j;
+                            pFinal[1] = i;
+                        }
+                    }
+                }
+
+            }
+            Imprimir();
+            asignarPosiciones();
+        }
+
+        public void Imprimir() {
+            string sImpresion = "";
+            int iCont = 0;
+            foreach (char item in aMapa)
+            {
+                sImpresion += item + " ";
+                iCont++;
+                if (iCont == 10)
+                {
+                    sImpresion += "\r\n";
+                    iCont = 0;
+                }
+            }
+            txtbxLaberinto.Text = sImpresion;
+            lblTurno.Text = "Turno actual: " + iTurno.ToString();
+        }
+
+        public void asignarPosiciones()
+        {
+            if (pJugador[0] == pInicial[0] && pJugador[1] == pInicial[1])
+            {
+                aMapa[pInicial[0], pInicial[1]] = 'I';
+            }
+            
+
+
+      
+           
+            for (int j = 0; j <= 9; j++)
+            {
+                for (int i = 0; i <= 9; i++)
+                {
+                    if (aMapa[j, i] == 'I')
+                    {
+                        pInicial[0] = j;
+                        pInicial[1] = i;
+                    }
+                    else if (aMapa[j, i] == 'J')
+                    {
+                        pJugador[0] = j;
+                        pJugador[1] = i;
+                    }
+                    else if (aMapa[j, i] == 'R')
+                    {
+                        pReinicio[0] = j;
+                        pReinicio[1] = i;
+                        
+                    }
+                    else if (aMapa[i, j] == 'F')
+                    {
+                        pFinal[0] = j;
+                        pFinal[1] = i;
+                       
+                        
+                    }
+                  
+                   
+                }
+               
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            aMapa[pInicial[0], pInicial[1]] = 'J';
+            pJugador[0] = pInicial[0];
+            pJugador[1] = pInicial[1];
+            btnStart.Enabled = false;
+            Imprimir();
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (iTurno == maxTurnos)
+            {
+                MessageBox.Show(" \n No hay mas turnos.");                
+                Application.Restart();
+            }
+            if (e.KeyCode == Keys.Right)
+            {            
+                    if (aMapa[pJugador[0], pJugador[1] + 1] != 'X')
+                {
+                    iTurno++;
+                    while (aMapa[pJugador[0], pJugador[1] + 1] != 'X')
+                    {
+                        aMapa[pJugador[0], pJugador[1]] = ' ';
+                        aMapa[pJugador[0], pJugador[1] + 1] = 'J';
+                        Imprimir();
+                        asignarPosiciones();
+                        if (pJugador[1] + 1 > 9)
+                        {
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            pJugador[1] = 0;
+                            aMapa[pJugador[0], pJugador[1]] = 'J';
+                        }
+                        if (pJugador[0] == pReinicio[0] && pJugador[1] == pReinicio[1])
+                        {
+
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            aMapa[pInicial[0], pInicial[1]] = 'J';
+                            aMapa[pReinicio[0], pReinicio[1]] = 'R';
+                            Imprimir();
+                            asignarPosiciones();
+                         }
+                         if (pJugador[0] == pFinal[0] && pJugador[1] == pFinal[1])
+                        {
+                            MessageBox.Show("Felicidades! llegaste al final del laberinto\nTe tomo " + iTurno + " turnos llegar al final");
+                            Application.Restart();
+                        }
+                    }
+
+                }
+                
+               
+
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+
+                if (aMapa[pJugador[0], pJugador[1] - 1] != 'X')
+                {
+                    iTurno++;
+                    while (aMapa[pJugador[0], pJugador[1] - 1] != 'X')
+                    {
+                        aMapa[pJugador[0], pJugador[1]] = ' ';
+                        aMapa[pJugador[0], pJugador[1] - 1] = 'J';
+                        Imprimir();
+                        asignarPosiciones();
+                        if (pJugador[1] - 1 < 0)
+                        {
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            pJugador[1] = 9;
+                            aMapa[pJugador[0], pJugador[1]] = 'J';
+                        }
+                        if (pJugador[0] == pReinicio[0] && pJugador[1] == pReinicio[1])
+                        {
+
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            aMapa[pInicial[0], pInicial[1]] = 'J';
+                            aMapa[pReinicio[0], pReinicio[1]] = 'R';
+                            Imprimir();
+                            asignarPosiciones();
+                         }
+                         if (pJugador[0] == pFinal[0] && pJugador[1] == pFinal[1])
+                        {
+                            MessageBox.Show("Felicidades! llegaste al final del laberinto\nTe tomo " + iTurno + " turnos llegar al final");
+                            Application.Restart();
+                        }
+                    }
+                }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if(aMapa[pJugador[0] - 1, pJugador[1]] != 'X')
+                {
+                    iTurno++;
+                    while (aMapa[pJugador[0] - 1, pJugador[1]] != 'X')
+                {
+                   
+                    aMapa[pJugador[0], pJugador[1]] = ' ';
+                    aMapa[pJugador[0] - 1, pJugador[1]] = 'J';
+                    Imprimir();
+                    asignarPosiciones();
+                    if (pJugador[0] - 1 < 0)
+                    {
+                        aMapa[pJugador[0], pJugador[1]] = ' ';
+                        pJugador[0] = 9;
+                        aMapa[pJugador[0], pJugador[1]] = 'J';
+                    }
+                        if (pJugador[0] == pReinicio[0] && pJugador[1] == pReinicio[1])
+                        {
+
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            aMapa[pInicial[0], pInicial[1]] = 'J';
+                            aMapa[pReinicio[0], pReinicio[1]] = 'R';
+                            Imprimir();
+                            asignarPosiciones();
+                     }
+                        else if (pJugador[0] == pFinal[0] && pJugador[1] == pFinal[1])
+                        {
+                            MessageBox.Show("Felicidades! llegaste al final del laberinto\nTe tomo " + iTurno + " turnos llegar al final");
+                            Application.Restart();
+                        }
+                    }
+
+                }
+           
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (aMapa[pJugador[0] + 1, pJugador[1]] != 'X')
+                {
+                    iTurno++;
+                        while (aMapa[pJugador[0] + 1, pJugador[1]] != 'X')
+                    {
+                        aMapa[pJugador[0], pJugador[1]] = ' ';
+                        aMapa[pJugador[0] + 1, pJugador[1]] = 'J';
+                        Imprimir();
+                        asignarPosiciones();
+                        if (pJugador[0] + 1 > 9)
+                        {
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            pJugador[0] = 0;
+                            aMapa[pJugador[0], pJugador[1]] = 'J';
+                        }
+                        if (pJugador[0] == pReinicio[0] && pJugador[1] == pReinicio[1])
+                        {
+
+                            aMapa[pJugador[0], pJugador[1]] = ' ';
+                            aMapa[pInicial[0], pInicial[1]] = 'J';
+                            aMapa[pReinicio[0], pReinicio[1]] = 'R';
+                            Imprimir();
+                            asignarPosiciones();
+                        }
+                         if (pJugador[0] == pFinal[0] && pJugador[1] == pFinal[1])
+                        {
+                            MessageBox.Show("Felicidades! llegaste al final del laberinto\nTe tomo " + iTurno + " turnos llegar al final");
+                            Application.Restart();
+                        }
+                    }
+                }
+               
+            }
+        }
+                
+    }
+}
